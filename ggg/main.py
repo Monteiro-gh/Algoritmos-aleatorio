fbdce394b0f1b670dc9fd8ca20300c62341663a8
Monteiro-gh <161480035+Monteiro-gh@users.noreply.github.com>
2024-03-25 11:13:19 -0300

    Add files via upload

---
+++ ggg/main.py
@@ -0,0 +1,27 @@
+def Consulta_de_nome():
+
+    nome = input("Digite seu nome" )
+
+    if not nome:
+        nome = "você"
+
+    print(f"{nome} caso acerte ganhará um 1")
+
+
+
+def A_B():
+
+    A = int(input("digite um valor."))
+    B = int(input("digite outro valor."))
+
+    print(f"o primeiro valor é igual{B}. " )
+    print(f"o segundo valor é igual{A}. " )
+
+def CLT():
+
+    Salario = int(input(f"informe seu salário {nome}. "))
+    Reajuste = int(input("informe o aumento."))
+
+CLT()
+Consulta_de_nome()
+A_B()
